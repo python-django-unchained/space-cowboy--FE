32647c19ae1fed4bc97ce8500edc32145437b73f
Kevin Allen <kevin.allen91@gmail.com>
2020-03-02 13:39:51 -0800

    added navbar to main app file

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import logo from './logo.svg';
-import './App.css';
+
+// Components
+import Navbar from './components/landing-page'
+
+// Styles
+import './styles/App.scss';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        
+        <Navbar />
       </header>
     </div>
   );
